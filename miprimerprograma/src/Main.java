52fa95106cf4614407e21f5b033b2142685b38b3
Mirianne Santos <mirianneds@gmail.com>
2025-10-05 20:45:06 +0200

    update

---
+++ miprimerprograma/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    // Esto es un comentario
+    /* Este comentario
+    se puede dividir
+    en varias lineas
+    de codigo
+     */
+
+    //TODO me falta esto
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Esta es la segunda linea del programa");
+
+    }
+
+    public void nombreMetodo(){
+
+    }
+
+    //TODO esto también me falta
+}
