d5465e0028d6561ce7cd6547aa30e6e54ecaadd6
Mirianne Santos <mirianneds@gmail.com>
2025-10-12 15:45:44 +0200

    update

--- miprimerprograma/src/Main.java
+++ miprimerprograma/src/Main.java
@@ -8,9 +8,28 @@ public class Main {
      */
 
     //TODO me falta esto
+
     public static void main(String[] args){
         System.out.println("Este es mi primer programa en Java");
-        System.out.println("Esta es la segunda linea del programa");
+          String   miNombre = "Miri"; // null
+        char letraDNI = 'Y';
+        int edad = 27;
+        double altura = 1.71;
+        float peso = 70.76f;
+        boolean experiencia = true;
+
+        miNombre = "Mirianne dos Santos";
+        letraDNI = 'H';
+        experiencia = false;
+
+        miNombre = "Mirianne";
+        System.out.println("Mi nombre es: "+miNombre);
+        System.out.println("La letra de mi DNI es: "+letraDNI);
+        System.out.println("Mi edad es: "+edad);
+        System.out.println("Mi altura es de: "+altura+"cm");
+        System.out.println("Mi peso es: "+peso+"kg");
+        System.out.println("Mi experiencia es: "+experiencia);
+
 
     }
 
---
+++ miprimerprograma/src/MainMethod.java
@@ -0,0 +1,17 @@
+public class MainMethod {
+   public static void main(String[] args){
+        System.out.println("Main para brobar metodos y variables");
+        saludar("Miri");
+        despedir();
+    }
+
+   public static void saludar(String nombre ){
+        System.out.println("Enhorabuena "+data+" has llamado a tu primer metdo");
+    }
+
+   public static void despedir(){
+        System.out.println("Hasta luego, hemos terminado");
+    }
+
+
+}
